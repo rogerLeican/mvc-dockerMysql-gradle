f3700d1c889be8fffd5764c49a6086c215ec17a1
rogerLeican <roger.lucian.maciel@gmail.com>
2020-03-06 15:13:01 -0300

    iniciando o projeto

---
+++ src/main/java/br/com/technocorp/introspringmvc/IntroSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package br.com.technocorp.introspringmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroSpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroSpringMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/technocorp/introspringmvc/IntroSpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.technocorp.introspringmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+//class IntroSpringMvcApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
